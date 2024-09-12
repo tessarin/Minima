d01d7f884e9c453b20cffff2595f5919b5a1020d
Cesar Tessarin <cesar@tessarin.com.br>
2024-09-12 19:32:32 -0300

    Add response and request to Controller
    Both from Plack.

--- lib/Minima/Controller.pm
+++ lib/Minima/Controller.pm
@@ -3,24 +3,34 @@ use experimental 'class';
 
 class Minima::Controller;
 
+use Plack::Request;
+use Plack::Response;
+
 field $env      :param(environment);
 field $app      :param;
 field $route    :param = {};
 
+field $request;
+field $response;
+field $params;
+
+ADJUST {
+    $request  = Plack::Request->new($env);
+    $response = Plack::Response->new(200);
+    $response->content_type('text/plain; charset=utf-8');
+
+    $params = $request->parameters;
+}
+
 method home
 {
-    [
-        200,
-        [ 'Content-Type' => 'text/plain' ],
-        [ "hello, world\n" ]
-    ]
+    $response->body("hello, world\n");
+    $response->finalize;
 }
 
 method not_found
 {
-    [
-        404,
-        [ 'Content-Type' => 'text/plain' ],
-        [ "not found\n" ]
-    ]
+    $response->code(404);
+    $response->body("not found\n");
+    $response->finalize;
 }
