1cad2cb81fdec539551f26a44c816fe05615a478
Cesar Tessarin <cesar@tessarin.com.br>
2024-09-12 15:59:43 -0300

    Make it work
    This commit adds:
      - A Minimal .psgi file, to be able to test the distribution.
      - The Setup package.
      - A run method on App.

--- lib/Minima/App.pm
+++ lib/Minima/App.pm
@@ -2,3 +2,12 @@ use v5.40;
 use experimental 'class';
 
 class Minima::App;
+
+method run
+{
+    [
+        200,
+        [ 'Content-Type' => 'text/plain' ],
+        [ "hello, world\n" ]
+    ]
+}
