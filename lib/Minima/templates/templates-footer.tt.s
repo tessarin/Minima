261359e79a6711fdb4b38c54a6d774aca23202b6
Cesar Tessarin <cesar@tessarin.com.br>
2024-09-19 11:01:01 -0300

    Fix default template not including extras
    Header CSS and scripts, as well as footer scripts, were missing.

--- lib/Minima/templates/templates-footer.tt.s
+++ lib/Minima/templates/templates-footer.tt.s
@@ -3,5 +3,11 @@
 <footer>
 </footer>
 
+%% if view.scripts
+<script>
+%% foreach script in view.scripts ; include $script ; end
+</script>
+%% end
+
 </body>
 </html>
--- lib/Minima/templates/templates-header.tt.d
+++ lib/Minima/templates/templates-header.tt.d
@@ -13,9 +13,15 @@
 %% if view.settings.theme_color
   <meta name="theme-color" content="[% view.theme_color %]">
 %% end
+%% if view.header_css ; foreach css in view.header_css
+  <link rel="stylesheet" href="[% css %]" />
+%% end ; end
 @@ IF static
   <link rel="stylesheet" href="/css/main.css" />
 @@ END
+%% if view.header_scripts ; foreach script in view.header_scripts
+  <script src="[% script %]"></script>
+%% end ; end
 </head>
 <body>
 
