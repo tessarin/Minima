8b56f3174bd8c24188c6ba28e1fc8b14a797e84a
Cesar Tessarin <cesar@tessarin.com.br>
2024-09-19 20:42:26 -0300

    Add documentation

--- lib/Minima/Router.pm
+++ lib/Minima/Router.pm
@@ -57,3 +57,179 @@ method error_route
 {
     $special{server_error}
 }
+
+__END__
+
+=head1 NAME
+
+Minima::Router - Define and match URIs to controllers and methods
+
+=head1 SYNOPSIS
+
+    use Minima::Router;
+
+    my $router = Minima::Router->new;
+
+    $router->read_file('etc/routes.map');
+    my $match = $router->match($env); # Plack env
+
+    # unless ($match) ... handle a no match case
+
+    my $controller = $match->{controller};
+    my $method = $match->{action};
+
+    try {
+        $controller->$action;
+    } catch ($e) {
+        $match = $router->error_route;
+        # handle error
+    }
+
+=head1 DESCRIPTION
+
+Minima::Router is built on top of L<Router::Simple> and serves as a
+dispatcher for web applications, interfacing between a routes file and
+the router itself.
+
+This module parses a custom syntax (see below) for determining routes
+and is tightly integrated with the Minima framework, being used by
+L<Minima::App> automatically.
+
+=head2 Using This Module Outside Minima
+
+A matched route returns, by definition, a controller name and action in
+the form of a hash reference. This hash may contain more data, such as
+data extracted from the URI by L<Router::Simple>. The main controller
+and action keys may be undefined if not provided in the routes file.
+
+As long as your use case accepts this hash, there is nothing preventing
+you from using this module independently.
+
+=head1 ROUTES FILE
+
+=head2 Syntax
+
+A routes file contains one route per line and follows a rigid structure
+of four columns separated by whitespace (spaces or tabs):
+
+    <method or directive>  <route name>  <controller>  <action>
+
+Blank lines are discarded, and lines beginning with C<#> are considered
+comments and are also discarded. Your routes file may be placed anywhere
+and have any extension.
+
+=over 4
+
+=item B<Method or Directive>
+
+The name of the HTTP method to which this route applies (GET, POST,
+etc.). This value may also be set to C<*>, in which case any method is
+permitted for this route. If set to C<@>, the route represents a special
+directive determined in conjunction with the next column.
+
+=item B<Route Name>
+
+The name or pattern of this route, in any format understood by
+L<Router::Simple>. If this route is a directive, the currently accepted
+values for the route name and their meanings are:
+
+=over 4
+
+=item C<not_found>
+
+Registers the controller and action pair as the return value for cases
+where the router didn't find any valid match.
+
+=item C<server_error>
+
+Registers the controller and action pair as the return value for the
+method L<C<error_route>|/error_route>. B<Note:> Use a controller that
+requires minimal setup as your error handler. If your controller failed
+due to a database error, for instance, there is no point in trying to
+start it again just to show an error page.
+
+Minima::App will call the error method with an argument representing the
+exception. If desired, the method can utilize this argument.
+
+=back
+
+=item B<Controller>
+
+The name of the controller that will respond to this match, returned in
+the match hash reference with the key C<controller>. If the controller
+name starts with C<:>, then C<Controller:> will be automatically
+prepended.
+
+This may be left blank only if the next column is also blank, which will
+be translated as C<undef> in the match hash.
+
+=item B<Action>
+
+Name of the method that should be called on the controller to this
+match, returned in the match hash reference with the key C<action>.
+
+This may be left blank, which will be translated as C<undef> in the
+match hash.
+
+=back
+
+=head2 Example
+
+    # Main Routes
+    *       /               :Main         home
+    GET     /about          :Main         about_page
+    GET     /blog/{post}    Blog::Main    article
+
+    # Form processing
+    POST    /contact        :Form         contact
+
+    # Special
+    @       not_found       :Main         not_found_page
+    @       server_error    :Error        error_page
+
+=head1 METHODS
+
+=head2 read_file
+
+    method read_file ($file)
+
+Parses the routes file given as an argument and registers the routes.
+This method can be called multiple times on the same instance to process
+more than one file.
+
+=head2 match
+
+    method match ($env)
+
+Performs a match on the passed Plack C<$env>, or URI, and returnes a
+hash reference containing the controller-action pair as well as extra
+data extracted from the URI match.
+
+    { controller => '...', action => '...' }
+
+If no match is made and a not found route is registered (via the
+L<C<not_found>|/not_found> directive), its data is returned. If no match
+is found and no special directive is present in the routes file, it
+returns C<undef>.
+
+Note that this does not call the controller. It's up to the user to do
+that in order to perform the intended action.
+
+=head2 error_route
+
+    method error_route ()
+
+Returns the controller-action pair registered with the
+L<C<server_error>|/server_error> directive. If nothing was registered,
+returns C<undef>.
+
+=head1 SEE ALSO
+
+L<Minima>, L<Minima::Controller>, L<Router::Simple>, L<Plack>,
+L<perlclass>.
+
+=head1 AUTHOR
+
+Cesar Tessarin, <cesar@tessarin.com.br>.
+
+Written in September 2024.
--- lib/Minima/View/HTML.pm
+++ lib/Minima/View/HTML.pm
@@ -130,3 +130,247 @@ sub _ext ($file)
 {
     $file = "$file.tt" unless $file =~ /\.\w+$/;
 }
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Minima::View::HTML - Render HTML views
+
+=head1 SYNOPSIS
+
+    use Minima::View::HTML;
+
+    my $view = Minima::HTML::View->new(app => $app);
+
+    $view->set_directory('templates'); # where templates resite
+    $view->set_template('home');
+    $view->set_title('Minima');
+    $view->add_script('global.js');
+
+    my $body = $view->render({ data => ... });
+
+=head1 DESCRIPTION
+
+Minima::View::HTML provides a way to render HTML templates using
+L<Template Toolkit|Template> with ease and a versatile set of data
+and settings.
+
+This class holds a reference to a L<Minima::App> object, which is
+primarily used to interact with the configuration hash, where defaults
+may be set to customize its behaviour.
+
+Its principle of operation is quite simple: after configuring the
+directory where templates reside (with L<C<set_directory>> or the
+default F<templates> directory) and selecting the template to be used
+(with L<C<set_template>>), the view collects and formats data to pass to
+the template. This final stage ultimately determines how the output is
+structured.
+
+=head1 DATA
+
+=head2 Content
+
+The following data is managed and made available to template in the
+C<view> hash. Whether it's used for its original purpose or a custom one
+is left to the template implementation itself.
+
+=over 4
+
+=item C<title>
+
+Scalar used as the page title.
+
+=item C<description>
+
+Scalar used as the page description (C<E<lt>metaE<gt>> tag).
+
+=item C<header_scripts>
+
+A list of scripts to be included in the header.
+
+=item C<header_css>
+
+A list of linked CSS to be included in the header.
+
+=item C<pre>
+
+A list of templates to be included before the main template.
+
+=item C<post>
+
+A list of templates to be included after the main template.
+
+=item C<pre_body>
+
+A list of templates to be included right before the opening
+C<E<lt>bodyE<gt>> tag.
+
+=item C<scripts>
+
+A list of scripts to be embeded directly at the end of
+C<E<lt>bodyE<gt>>.
+
+=item C<classes>
+
+A list of CSS classes to be included in C<E<lt>mainE<gt>>.
+
+=back
+
+=head2 Settings
+
+The following data is managed and made available to the template in the
+C<view.settings> hash.
+
+=over 4
+
+=item C<block_indexing>
+
+A boolean scalar holding whether or not robots should be blocked from
+indexing the page.
+
+=item C<theme_color>
+
+A color to be set on the C<E<lt>meta name="theme-color"E<gt>> tag.
+
+=back
+
+=head1 CONFIGURATION
+
+The C<tt> key may be used in the main L<Minima::App> configuration hash
+to customize L<Template Toolkit|Template>.
+
+By default, the following configuration is used:
+
+    {
+        OUTLINE_TAG => '%%',
+        ANYCASE => 1,
+    }
+
+These can be overwritten. Additionally, if the app is in development
+mode (see L<Minima::App/development>), C<DEBUG> is set to
+C<DEBUG_UNDEF>.
+
+=head1 METHODS
+
+=head2 render
+
+    method render ($data = {})
+
+Renders the template with the passed data made available to it, as well
+as the standard data (described in L<"Data"|/DATA>) and returns it.
+
+To configure L<Template Toolkit|Template>, see the
+L<"Configuration"/CONFIGURATION> section.
+
+=head2 set_title
+
+    method set_title ($title, $description = undef)
+
+Sets the title and description (optional).
+
+=head2 set_compound_title
+
+    method set_compound_title ($title, $description = undef)
+
+Sets a secondary title, using the main title as primary, as well as
+description.
+
+    $v->set_title('Title');
+    $v->set_compound_title('Page');
+    # Results in: Title • Page
+
+If no primaty title is already set, calling this method produces the
+same effect as L<C<set_title>|/set_title>.
+
+=head2 set_directory
+
+    method set_directory ($directory)
+
+Sets the main directory where templates reside. If this method is not
+called, the default F<templates> directory will be used.
+
+=head2 set_template
+
+    method set_template ($title)
+
+Sets the template name to be used. If no extension is present, F<.tt>
+will be added.
+
+=head2 add_include_path
+
+    method add_include_path ($directory)
+
+Adds the passed directory as a include path in conjunction with the main
+directory (set by L<C<set_directory>|set_directory>). This method can be
+called multiple times to add multiple paths.
+
+=head2 set_block_indexing
+
+    method set_block_indexing ($bool = 1)
+
+Sets a boolean scalar to indicate if robots should be blocked from
+indexing the page. Defaults to true.
+
+=head2 set_name_as_class
+
+    method set_name_as_class ($bool = 1)
+
+Sets a boolean scalar to indicate whether the template name should be
+added to the C<E<lt>mainE<gt>> CSS class list. This is particularly
+useful to target a page on a CSS file by simply using C<.main.template>.
+Defaults to true.
+
+=head2 add_header_script
+
+    method add_header_script ($script)
+
+Adds the passed script to the header script list.
+
+=head2 add_header_css
+
+    method add_header_css ($css)
+
+Adds the passed CSS file name to the header CSS list.
+
+=head2 add_pre
+
+    method add_pre ($pre)
+
+Adds the passed template name to the pre-template list.
+
+=head2 add_post
+
+    method add_post ($post)
+
+Adds the passed template name to the post-template list.
+
+=head2 add_pre_body
+
+    method add_pre_body ($pre)
+
+Adds the passed template name to the pre-body template list.
+
+=head2 add_script
+
+    method add_script ($script)
+
+Adds the passed script name to the list of scripts embedded in the body.
+
+=head2 add_class
+
+    method add_class ($class)
+
+Adds the passed class name to the list of C<E<lt>mainE<gt>> classes.
+
+=head1 SEE ALSO
+
+L<Minima>, L<Minima::Controller>, L<perlclass>.
+
+=head1 AUTHOR
+
+Cesar Tessarin, <cesar@tessarin.com.br>.
+
+Written in September 2024.
